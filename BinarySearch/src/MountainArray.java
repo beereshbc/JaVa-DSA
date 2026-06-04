ff78476195c2e0d99e8f44c0b715a5e18d17cb82
beereshbc <bcbeereshkumar@gmail.com>
2026-06-04 23:25:11 +0530

    sorting Bubble sort

--- BinarySearch/src/MountainArray.java
+++ BinarySearch/src/MountainArray.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
-public class MountainArray {
-}
+//public class MountainArray {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//        public int findInMountainArray(int target, MountainArray mountainArr) {
+//
+//            int peak = findPeak(mountainArr);
+//
+//            int firstTry = binarySearch(mountainArr, target, 0, peak, true);
+//            if (firstTry != -1) return firstTry;
+//
+//            return binarySearch(mountainArr, target, peak + 1, mountainArr.length() - 1, false);
+//        }
+//
+//        private int findPeak(MountainArray arr){
+//            int start = 0;
+//            int end = arr.length() - 1;
+//
+//            while (start < end) {
+//                int mid = start + (end - start) / 2;
+//
+//                if (arr.get(mid) > arr.get(mid + 1)) {
+//                    end = mid;
+//                } else {
+//                    start = mid + 1;
+//                }
+//            }
+//            return start;
+//        }}
--- BinarySearch/src/RowColMatrix.java
+++ BinarySearch/src/RowColMatrix.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class RowColMatrix {
+    public static void main(String[] args) {
+int[][] mat = {
+        {10,20,30,40},
+        {15,25,35,45},
+        {28,29,37,49},
+        {33,34,38,50}
+};
+        System.out.println(Arrays.toString(search(mat, 37)));
+    }
+
+    static int[] search(int[][] matrix, int target){
+        int row =0;
+        int col = matrix.length-1;
+
+        while (row < matrix.length && col >=0){
+            if (matrix[row][col] == target){
+                return new int[]{row, col};
+            }
+            if (matrix[row][col]<target){
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
 }
