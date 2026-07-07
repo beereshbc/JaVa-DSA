1bcac7b0f08c31e42a2135276887485d9085b34a
beereshbc <bcbeereshkumar@gmail.com>
2026-07-07 20:42:38 +0530

    Factors method

---
+++ BitOperator/src/Factors.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Factors {
+    static void main() {
+        factor2(36);
+    }
+//Factorization but takes more Time O(n)
+    static void factor1(int n){
+        for (int i = 1; i <=n ; i++) {
+            if (n%i == 0){
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
+    static void factor2(int n){
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <=Math.sqrt(n) ; i++) {
+            if (n%i == 0){
+                if (i == n/i){
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + " ");
+                    list.add(n/i);
+                }
+            }
+
+        }
+
+        for (int i = list.size()-1; i >=0 ; i--) {
+            System.out.print( list.get(i) + " ");
+        }
+    }
+
+}
