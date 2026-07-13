2d000b6d25380e8f5acf8440e566dc4770c04465
beereshbc <bcbeereshkumar@gmail.com>
2026-07-13 17:04:07 +0530

    selection sort

--- Recursion/src/SelectionSort.java
+++ Recursion/src/SelectionSort.java
@@ -1,16 +1,28 @@
+import java.util.Arrays;
+
 public class SelectionSort {
     static void main(String[] args) {
-
+ int[] arr = {4,3,2,1};
+        selectionSort(arr, 0, arr.length, 0);
+        System.out.println(Arrays.toString(arr));
     }
 
-    static void selectionSort(int[] arr, int s, int e){
-        if (e==0){
-            return;
+    static void selectionSort(int[] arr, int s, int e, int max) {
+    if (e==0){
+        return;
+    }
+    if (s<e){
+        if (arr[s]>arr[max]){
+            selectionSort(arr, s+1, e, s);
+        } else {
+            selectionSort(arr, s+1, e, max);
         }
+    } else {
+        int temp = arr[max];
+        arr[max] = arr[e-1];
+        arr[e-1] = temp;
+        selectionSort(arr, 0, e-1,0);
+    }
 
-        int large;
-        if (e<s){
-
-        }
     }
 }
