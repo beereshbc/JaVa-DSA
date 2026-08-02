b5f1ef1cb00665822cd80a87cf237bc2d2da90ff
beereshbc <bcbeereshkumar@gmail.com>
2026-08-02 22:29:41 +0530

    LC-35

---
+++ LC/src/LC35.java
@@ -0,0 +1,29 @@
+public class LC35 {
+
+    public static void main(String[] args) {
+        int[] arr =  {1,3,5,6};
+        int target = 7;
+       int ans = searchInsert(arr, target);
+        System.out.println(ans);
+    }
+
+
+
+    static int searchInsert(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if (target < nums[mid]){
+                end=mid-1;
+            } else if(target>nums[mid]) {
+                start = mid+1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
