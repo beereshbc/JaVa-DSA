672744c5582699ca56941becbfb8727717adaf3d
beereshbc <bcbeereshkumar@gmail.com>
2026-08-18 23:35:36 +0530

    String problems using recursion

---
+++ Recursion/src/StringProblems.java
@@ -0,0 +1,26 @@
+public class StringProblems {
+
+   public static void main(String[] args) {
+
+skip("", "abbghahgai");
+    }
+
+    static void skip(String p, String up) {
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+
+        char ch = up.charAt(0);
+
+        if(ch == 'a'){
+            skip(p, up.substring(1));
+        } else {
+            skip(p+ch, up.substring(1));
+        }
+
+
+
+
+    }}
