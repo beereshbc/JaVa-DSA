ddd4b9784fa068e3d47d620b69fe2047284db6c8
beereshbc <bcbeereshkumar@gmail.com>
2026-09-01 22:38:23 +0530

    217 Leetcode

---
+++ Recursion/src/phonePad.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class phonePad {
+
+    public static void main(String[] args) {
+
+    }
+
+    static ArrayList<String> pad(String p, String up){
+        if (up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        
+        int digit = up.charAt(0)-'0';
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = (digit - 1)*3; i < digit*3; i++) {
+            char ch = (char) ('a'+i);
+           list.addAll(pad(p+ch, up.substring(1)));
+
+        }
+        return list;
+        
+    }
+}
