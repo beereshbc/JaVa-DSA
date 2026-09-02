bb95b9debae64871486c6770e1fd32a4f6af2b3e
beereshbc <bcbeereshkumar@gmail.com>
2026-09-02 13:53:37 +0530

    LC 242 anagram solved

---
+++ NeetCode/src/LC242.java
@@ -0,0 +1,32 @@
+public class LC242 {
+   public static void main(String[] args) {
+       System.out.println(
+               isAnagram("kannada", "kandana")
+       );
+    }
+
+    static boolean isAnagram(String s, String t) {
+
+       if (s.length() != t.length()) {
+           return false;
+       }
+
+           int[] charCounts = new int[26];
+
+           for (int i = 0; i < s.length(); i++) {
+
+               charCounts[s.charAt(i)-'a']++;
+               charCounts[t.charAt(i)-'a']--;
+
+           }
+
+           for (int count : charCounts){
+               if (count != 0){
+                   return false;
+               }
+           }
+
+
+        return true;
+    }
+}
